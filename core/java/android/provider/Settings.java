7e6efff451b15d75050f81c75f55339b688decbc
Michael Bestas <mkbestas@lineageos.org>
2018-01-21 00:22:10 +0200

    frameworks: Add support for advanced restart options
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Dave Kessler <activethrasher00@gmail.com>
    Co-authored-by: DvTonder <david.vantonder@gmail.com>
    Co-authored-by: Jorge Ruesga <jorge@ruesga.com>
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Co-authored-by: Roman Birg <roman@cyngn.com>
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Co-authored-by: Veeti Paananen <veeti.paananen@rojekti.fi>
    Co-authored-by: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Change-Id: I4b35d10b0811eff0d8e9ab68b2aedbe52b4aa53f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7098,6 +7098,13 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Whether to enable advanced reboot
+         * @hide
+         */
+        @Readable
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
