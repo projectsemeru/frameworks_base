9e4b752a921450ae05a1d99f08b97bd267f24937
jhenrique09 <jhsv09@gmail.com>
2022-02-12 17:46:32 -0300

    KeyguardBouncer: Don't delay showing if face auth running
    [someone5678] Forward port to A13 QPR3
    
    Change-Id: I1897aa22a9c29627b2f03e14f093151453df7be4

--- packages/SystemUI/src/com/android/systemui/bouncer/domain/interactor/PrimaryBouncerInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/bouncer/domain/interactor/PrimaryBouncerInteractor.kt
@@ -79,9 +79,6 @@ constructor(
     private val selectedUserInteractor: SelectedUserInteractor,
     private val deviceEntryFaceAuthInteractor: DeviceEntryFaceAuthInteractor,
 ) {
-    private val passiveAuthBouncerDelay =
-        context.resources.getInteger(R.integer.primary_bouncer_passive_auth_delay).toLong()
-
     /** Runnable to show the primary bouncer. */
     val showRunnable = Runnable {
         repository.setPrimaryShow(true)
@@ -179,12 +176,7 @@ constructor(
 
             Log.i(TAG, "Show primary bouncer requested, reason: $reason")
             repository.setPrimaryShowingSoon(true)
-            if (usePrimaryBouncerPassiveAuthDelay()) {
-                Log.d(TAG, "delay bouncer, passive auth may succeed")
-                mainHandler.postDelayed(showRunnable, passiveAuthBouncerDelay)
-            } else {
-                DejankUtils.postAfterTraversal(showRunnable)
-            }
+            DejankUtils.postAfterTraversal(showRunnable)
             keyguardStateController.notifyPrimaryBouncerShowing(true)
             primaryBouncerCallbackInteractor.dispatchStartingToShow()
             return true
