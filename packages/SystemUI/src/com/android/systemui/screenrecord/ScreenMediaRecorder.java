8ffae0bb1f5a314df50810c25ff9c356f32eb973
Ido Ben-Hur <idoybh2@gmail.com>
2022-10-07 02:11:14 +0300

    Screenrecord: Add an option to use HEVC
    Chose the profiles that best fit the ones we have in H.264 by spec
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I153858bee5b3cce1e4cac4f86f44586a272de6ab

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -108,6 +108,7 @@ public class ScreenMediaRecorder {
 
     private boolean mLowQuality;
     private boolean mLongerDuration;
+    private boolean mHEVC;
 
     private Context mContext;
     ScreenMediaRecorderListener mListener;
@@ -121,7 +122,7 @@ public class ScreenMediaRecorder {
             int displayId,
             ScreenMediaRecorderListener listener) {
         this(context, handler, uid, audioSource, captureRegion,
-                displayId, listener, false, false);
+                displayId, listener, false, false, false);
     }
 
     public ScreenMediaRecorder(
@@ -133,7 +134,8 @@ public class ScreenMediaRecorder {
             int displayId,
             ScreenMediaRecorderListener listener,
             boolean lowQuality,
-            boolean longerDuration) {
+            boolean longerDuration,
+            boolean hevc) {
         mContext = context;
         mHandler = handler;
         mUid = uid;
@@ -143,6 +145,7 @@ public class ScreenMediaRecorder {
         mDisplayId = displayId;
         mLowQuality = lowQuality;
         mLongerDuration = longerDuration;
+        mHEVC = hevc;
         mMaxRefreshRate = mContext.getResources().getInteger(
                 com.android.systemui.res.R.integer.config_screenRecorderMaxFramerate);
         mAvcProfileLevel = mContext.getResources().getString(
@@ -155,6 +158,10 @@ public class ScreenMediaRecorder {
 
     public void setLongerDuration(boolean longer) {
         mLongerDuration = longer;
+    }
+
+    public void setHEVC(boolean hevc) {
+        mHEVC = hevc;
     }
 
     private void prepare() throws IOException, RemoteException, RuntimeException {
@@ -206,12 +213,21 @@ public class ScreenMediaRecorder {
         }
         VideoParameters videoParameters = getSupportedSize(metrics.widthPixels,
                 metrics.heightPixels, refreshRate);
-        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
-        mMediaRecorder.setVideoEncodingProfileLevel(
-                MediaCodecInfo.CodecProfileLevel.AVCProfileMain,
-                mLowQuality
-                        ? MediaCodecInfo.CodecProfileLevel.AVCLevel32
-                        : getAvcProfileLevelCodeByName(mAvcProfileLevel));
+        if (!mHEVC) {
+            mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mMediaRecorder.setVideoEncodingProfileLevel(
+                    MediaCodecInfo.CodecProfileLevel.AVCProfileMain,
+                    mLowQuality
+                            ? MediaCodecInfo.CodecProfileLevel.AVCLevel32
+                            : getAvcProfileLevelCodeByName(mAvcProfileLevel));
+        } else {
+            mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.HEVC);
+            mMediaRecorder.setVideoEncodingProfileLevel(
+                    MediaCodecInfo.CodecProfileLevel.HEVCProfileMain,
+                    mLowQuality
+                            ? MediaCodecInfo.CodecProfileLevel.HEVCHighTierLevel31
+                            : MediaCodecInfo.CodecProfileLevel.HEVCHighTierLevel41);
+        }
         mMediaRecorder.setVideoSize(videoParameters.mWidth, videoParameters.mHeight);
         mMediaRecorder.setVideoFrameRate(videoParameters.mRefreshRate);
         mMediaRecorder.setVideoEncodingBitRate(mLowQuality ? (videoParameters.bitrate() / 3)
@@ -287,7 +303,7 @@ public class ScreenMediaRecorder {
     private VideoParameters getSupportedSize(final int screenWidth, final int screenHeight,
             int refreshRate)
             throws IOException {
-        String videoType = MediaFormat.MIMETYPE_VIDEO_AVC;
+        String videoType = mHEVC ? MediaFormat.MIMETYPE_VIDEO_HEVC : MediaFormat.MIMETYPE_VIDEO_AVC;
 
         // Get max size from the decoder, to ensure recordings will be playable on device
         MediaCodec decoder = MediaCodec.createDecoderByType(videoType);
