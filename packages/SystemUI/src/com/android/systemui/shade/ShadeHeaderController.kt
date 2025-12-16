9f2cc0cd3d72a8a5aecbb3b7b98789d2e2ca661f
Ido Ben-Hur <idoybh2@gmail.com>
2025-12-16 02:46:38 +0200

    SystemUI: Make battery clickable again
    Change-Id: I66751353944b4cf8a5b5827c3ce53129ec84f1e7

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -364,6 +364,7 @@ constructor(
         mView.requireViewById<ViewGroup>(R.id.hover_system_icons_container).apply {
             addView(batteryComposeView, -1)
         }
+        batteryComposeView.setOnClickListener { launchBatteryActivity() }
 
         carrierIconSlots =
             listOf(
@@ -463,6 +464,12 @@ constructor(
             .setInterpolator(if (show) Interpolators.ALPHA_OUT else Interpolators.ALPHA_IN)
             .setListener(CustomizerAnimationListener(show))
             .start()
+    }
+
+    private fun launchBatteryActivity() {
+        activityStarter.postStartActivityDismissingKeyguard(
+            Intent(Intent.ACTION_POWER_USAGE_SUMMARY), 0
+        )
     }
 
     @VisibleForTesting
